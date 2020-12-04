71f7914cfbec3bab258312114537c306e3a9e625
manzaloros <zacharymansellcello@gmail.com>
2020-12-04 17:00:02 -0600

    Add GET POST PUT DELETE routes and knex

--- index.js
+++ index.js
@@ -1,26 +1,71 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const { Pool, Client } = require('pg');
-const client = new Client();
-const pool = new Pool({
-  user: 'zacharymansell',
-  host: 'localhost',
-  database: 'express_test_db',
+
+app.use(express.json());
+
+const pg = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'zacharymansell',
+    database: 'express_test_db',
+  }
 });
 
-pool.query('select all', (err, res) => {
-  console.log("error:", err, 'result:', res);
-  pool.end();
+app.get('/customers', (req, res) => {
+  pg('customers').select().from('customers')
+    .then((results) => {
+      console.log('results from pg:', results)
+      res.send(results);
+    });
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello from App!');
+app.post('/customers', (req, res) => {
+  pg('customers').insert(req.body)
+    .then(results => {
+      console.log('results from insert: ', results);
+    })
+    .catch(err => {
+      console.error('Caught err:', err);
+      res.status(500);
+    })
+    .finally(() => {
+      res.end();
+    })
 });
 
-app.post('/customers/:id', (req, res) => {
+app.put('/customers/:id', (req, res) => {
+  pg('customers')
+    .where('id', '=', req.params.id)
+    .update({ name: req.body.name })
+    .then((results) => {
+      console.log('results of update: ', results);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(500);
+    })
+    .finally(() => {
+      res.end();
+    })
+});
 
-})
+app.delete('/customers/:id', (req, res) => {
+  pg('customers')
+    .where('id', '=', req.params.id)
+    .del()
+    .then((results) => {
+      console.log('results of update: ', results);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(500);
+    })
+    .finally(() => {
+      res.end();
+    })
+});
 
 app.listen(port, () => {
   console.log(`App listening on ${port}`);
